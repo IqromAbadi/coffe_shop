673228a548906cb26b83d07fe56c1259b97cdd06
Iqrom Abadi <iqromabadi15@gmail.com>
2024-09-06 20:32:40 +0700

    fix navbar dan tabbbar

---
+++ lib/app/data/allcoffe.dart
@@ -0,0 +1,11 @@
+class AllCoffe {
+  final String name;
+  final String imageUrl;
+  final double price;
+
+  AllCoffe({
+    required this.name,
+    required this.imageUrl,
+    required this.price,
+  });
+}
--- lib/app/modules/AllCoffe/controllers/all_coffe_controller.dart
+++ lib/app/modules/AllCoffe/controllers/all_coffe_controller.dart
@@ -1,23 +1,46 @@
+import 'package:coffe/app/data/allcoffe.dart';
 import 'package:get/get.dart';
 
 class AllCoffeController extends GetxController {
-  //TODO: Implement AllCoffeController
+  // List of products
+  var products = <AllCoffe>[].obs;
+  var isLoading = true.obs;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
+    fetchAllCoffe();
   }
 
-  @override
-  void onReady() {
-    super.onReady();
-  }
+  // Simulate fetching data from assets
+  void fetchAllCoffe() async {
+    await Future.delayed(
+        const Duration(seconds: 2)); // Simulate a network delay
+    var fetchAllCoffe = [
+      AllCoffe(
+        name: "Latte",
+        imageUrl: "assets/images/coffe1.png",
+        price: 4.5,
+      ),
+      AllCoffe(
+        name: "Cappuccino",
+        imageUrl: "assets/images/coffe2.png",
+        price: 4.0,
+      ),
+      AllCoffe(
+        name: "Espresso",
+        imageUrl: "assets/images/coffe3.png",
+        price: 3.0,
+      ),
+      AllCoffe(
+        name: "Americano",
+        imageUrl: "assets/images/coffe4.png",
+        price: 3.5,
+      ),
+    ];
 
-  @override
-  void onClose() {
-    super.onClose();
+    // Assign fetched products to the products list
+    products.value = fetchAllCoffe;
+    isLoading.value = false;
   }
-
-  void increment() => count.value++;
 }
